504f1fc0ed1f73280ac2b9bb0e41698f2e707399
heroleo001 <leo.heriban@gmail.com>
2022-01-23 12:31:17 +0100

    added teleport function to DrillItem

--- src/main/java/net/leo/weebquirks/item/custom/DrillItem.java
+++ src/main/java/net/leo/weebquirks/item/custom/DrillItem.java
@@ -2,8 +2,15 @@ package net.leo.weebquirks.item.custom;
 
 import net.leo.weebquirks.item.ModItemTier;
 import net.leo.weebquirks.util.ModTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.item.DiggerItem;
-import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class DrillItem extends DiggerItem {
     public DrillItem(Properties pProperties) {
@@ -11,4 +18,15 @@ public class DrillItem extends DiggerItem {
     }
 
 
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if (!pLevel.isClientSide) {
+            ThrownEnderpearl thrownenderpearl = new ThrownEnderpearl(pLevel, pPlayer);
+            thrownenderpearl.setItem(pPlayer.getItemInHand(pUsedHand));
+            thrownenderpearl.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 1.0F);
+            pLevel.addFreshEntity(thrownenderpearl);
+            pPlayer.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, (5*20), 20));
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
 }
