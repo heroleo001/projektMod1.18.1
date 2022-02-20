051f199004d1f4fe11955dfe6c8b8e95a0cea086
heroleo001 <leo.heriban@gmail.com>
2022-02-20 09:52:51 +0100

    helper functions for activation

--- src/main/java/net/leo/weebquirks/util/AbilitySetters.java
+++ src/main/java/net/leo/weebquirks/util/AbilitySetters.java
@@ -3,10 +3,12 @@ package net.leo.weebquirks.util;
 import net.leo.weebquirks.WeebQuirks;
 import net.leo.weebquirks.item.ModItems;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.world.NoteBlockEvent;
 
 public class AbilitySetters {
     public static int eraseAbility(Player player){
@@ -45,7 +47,9 @@ public class AbilitySetters {
 
             for (int i = 0; i < player.getInventory().getContainerSize(); ++i) {
                 Item current = player.getInventory().getItem(i).getItem();
-                if (current == ModItems.UPGRADER.get() || current == ModItems.TECHNIUM_DRILL.get() || current == ModItems.ONE_PUNCH_SWORD.get()){
+                if (current == ModItems.UPGRADER.get()
+                        || current == ModItems.TECHNIUM_DRILL.get()
+                        || current == ModItems.ONE_PUNCH_SWORD.get()){
                     player.getInventory().removeItem(i, 1);
                 }
             }
@@ -60,6 +64,8 @@ public class AbilitySetters {
             player.removeEffect(MobEffects.HEALTH_BOOST);
             player.removeEffect(MobEffects.DAMAGE_BOOST);
             player.removeEffect(MobEffects.MOVEMENT_SPEED);
+            player.removeEffect(MobEffects.DOLPHINS_GRACE);
+            player.removeEffect(MobEffects.JUMP);
             player.getPersistentData().putBoolean(WeebQuirks.MOD_ID + "flameActive", false);
             WeebQuirks.createLog("Ability Erased.");
             System.out.println("5");
@@ -68,17 +74,89 @@ public class AbilitySetters {
         return 1;
     }
 
-    public static int setSimonAbility(Player player){
+    public static void setSimonAbility(Player player){
         eraseAbility(player);
 
         player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
                 new int[]{ 1 });
         //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
-        WeebQuirks.createLog("Ability set");
+        WeebQuirks.createLog("Ability set to simon");
         WeebQuirks.setSimonPlayerEntity(((ServerPlayer) player));
         ItemStack drill = new ItemStack(ModItems.TECHNIUM_DRILL.get());
         player.getInventory().add(drill);
 
-        return 1;
+    }
+
+    public static void setErenAbility(Player player){
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 2 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set to eren");
+        WeebQuirks.setErenPlayerEntity(((ServerPlayer) player));
+
+
+    }
+
+    public static void setSinraAbility(Player player){
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 3 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set to sinra");
+        WeebQuirks.setSinraPlayerEntity(((ServerPlayer) player));
+        player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 2147483646, 10, false, false));
+
+
+    }
+
+    public static void setKorosenseiAbility(Player player){
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 5 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set korosensei");
+        WeebQuirks.setKorosenseiPlayerEntity(((ServerPlayer) player));
+        player.getPersistentData().putBoolean(WeebQuirks.MOD_ID + "speedActive", false);
+
+    }
+
+    public static void setSenkuAbility(Player player) {
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 4 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set to senku");
+        WeebQuirks.setSenkuPlayerEntity(((ServerPlayer) player));
+        ItemStack drill = new ItemStack(ModItems.UPGRADER.get());
+        player.getInventory().add(drill);
+    }
+
+    public static void setOnepunchmanAbility(Player player) {
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 6 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set to onepunchman");
+        WeebQuirks.setOnepunchmanPlayerEntity(((ServerPlayer) player));
+        ItemStack drill = new ItemStack(ModItems.ONE_PUNCH_SWORD.get());
+        player.getInventory().add(drill);
+    }
+
+    public static void setSubaruAbility(Player player) {
+        eraseAbility(player);
+
+        player.getPersistentData().putIntArray(WeebQuirks.MOD_ID + "ability",
+                new int[]{ 7 });
+        //LIST  Abilities:  Simon=1,  Eren=2, Sinra=3
+        WeebQuirks.createLog("Ability set to subaru");
+        WeebQuirks.setSubaruPlayerEntity(((ServerPlayer) player));
+        // ItemStack drill = new ItemStack(ModItems.TECHNIUM_DRILL.get());
+        // player.getInventory().add(drill);
     }
 }
